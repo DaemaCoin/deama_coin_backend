19e48d40096708419d8bc35131cac23cd1936daf
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-12 16:22:46 +0900

    프로젝트 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        GITHUB_CLIENT_ID: Joi.string().required(),
+        GITHUB_SECRET_ID: Joi.string().required(),
+        GITHUB_LOGIN_URL: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const port = process.env.HTTP_PORT;
+  await app.listen(port, () => {
+    Logger.log(`Server running on port ${port}`);
+  });
 }
 bootstrap();
