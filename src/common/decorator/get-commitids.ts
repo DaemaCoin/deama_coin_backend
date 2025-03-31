706e54a5a06c0e103da3f5a816db3c360b5307cd
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-31 14:49:09 +0900

    fix(get-commitids.ts): 커밋 ID 반환 로직 주석 처리 주석 처리된 코드는 향후 수정 및 검토를 위해 남겨둠

--- src/common/decorator/get-commitids.ts
+++ src/common/decorator/get-commitids.ts
@@ -3,6 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const GetCommitIds = createParamDecorator(
   (data, context: ExecutionContext): string[] => {
     const req = context.switchToHttp().getRequest();
-    return req.body.commits.map((value) => value.id);
+    return req
+    // return req.body.commits.map((value) => value.id);
   },
 );
