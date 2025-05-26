7f8de744218281e84d491ebb68f10e477fb4cefa
ljyo2o9 <yggosxoljy@gmail.com>
2025-05-26 16:01:33 +0900

    LOG

--- src/common/decorator/get-commit-ids.ts
+++ src/common/decorator/get-commit-ids.ts
@@ -3,6 +3,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const GetCommitIds = createParamDecorator<string[]>(
   (data: any, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    console.log('\n---COMMITS - BODY ---\n');
+    console.log(request.body);
+    
     return request.body.commits.map((commit) => commit.id);
   },
 );
