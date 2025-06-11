89965b89547e07a853c521f286051b2c649593eb
ljyo2o9 <yggosxoljy@gmail.com>
2025-06-11 14:35:40 +0900

    ERROR: 수정

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { StoreModule } from '../store/store.module';
+import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
-  imports: [StoreModule],
+  imports: [StoreModule, WalletModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
