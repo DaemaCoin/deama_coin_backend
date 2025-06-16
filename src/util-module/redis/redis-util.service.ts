f0e8cf85491977caa7bdcf060d3c9ac8a5603524
ljyo2o9 <yggosxoljy@gmail.com>
2025-06-16 15:47:19 +0900

    RedisUtilService delete, deleteByPattern 구현

--- src/util-module/redis/redis-util.service.ts
+++ src/util-module/redis/redis-util.service.ts
@@ -1,6 +1,7 @@
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
+
 @Injectable()
 export class RedisUtilService {
   constructor(
@@ -12,7 +13,22 @@ export class RedisUtilService {
     return await this.redisClient.get(key);
   }
 
-  async set(key: string, value: string) {
+  async set(key: string, value: string, ttlSeconds?: number) {
+    if (ttlSeconds) {
+      return await this.redisClient.set(key, value, 'EX', ttlSeconds);
+    }
     return await this.redisClient.set(key, value);
+  }
+
+  async delete(key: string): Promise<number> {
+    return await this.redisClient.del(key);
+  }
+
+  async deleteByPattern(pattern: string): Promise<number> {
+    const keys = await this.redisClient.keys(pattern);
+    if (keys.length > 0) {
+      return await this.redisClient.del(...keys);
+    }
+    return 0;
   }
 }
